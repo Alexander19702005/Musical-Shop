ee8c7c92ab6099cf3bcb0ad3b1c08c247c19ac66
Alexander19702005 <asapogov@mail.ru>
2024-08-03 12:52:25 +0500

    Add files via upload

---
+++ Select.sql
@@ -0,0 +1,40 @@
+SELECT name,duration_sec
+FROM song
+WHERE duration_sec = (
+    SELECT max(duration_sec)
+    FROM song
+);
+
+SELECT name,duration_sec
+FROM song
+WHERE duration_sec >= 210;
+
+SELECT name,year
+FROM sbornik
+WHERE year <='2018' and year <= '2020';
+
+SELECT name 
+FROM  artist
+WHERE length(name) = length(replace(name, ' ', ''));
+
+
+SELECT name 
+FROM  song
+WHERE name like '%Мой%' or name like '%my%' or name like 'мой' ;
+
+select name,count(*) from genre g
+join genre_artist ga on ga.genre_id = g.id
+GROUP BY g.name;
+
+select name,count(*) from song s
+left join album a on a.id = s.album
+where a."year" <= '2020'
+GROUP BY s.name;
+
+select  a.id,a.title,a.year,s.name,s.year ,s.duration_sec from album a 
+join song s on a.id=s.album; 
+select avg(duration_sec)
+WHERE mot distinct title   
+
+select avg(duration_sec) from song s 
+
